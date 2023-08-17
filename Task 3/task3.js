168f46bd01d339aa0a99c9b69c583c1cbbc4f796
Hassaan1969 <tasleem2003@gmail.com>
2023-08-17 13:23:30 -0700

    task 3

---
+++ Task 3/task3.js
@@ -0,0 +1,5 @@
+"use strict";
+let z = "Muhammad Hassaan";
+console.log(z.toLowerCase());
+console.log(z.toUpperCase());
+console.log("in title case the name will", z);
---
+++ Task 3/task3.ts
@@ -0,0 +1,4 @@
+let z="Muhammad Hassaan";
+console.log(z.toLowerCase());
+console.log(z.toUpperCase());
+console.log("in title case the name will" ,z);
